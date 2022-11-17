0a07c099a9b5ce4c6eecd7fba11d3624a4272654
Aleksei <zombehalk@gmail.com>
2022-11-17 20:54:34 +0700

    burger adding commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.menu-btn')
+let menu = document.querySelector('.mobile-navigation')
+menuBtn.addEventListener('click', function(){
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active');
+});
