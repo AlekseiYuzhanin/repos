80859f1d6a9dbe3e892e44a97174a0e756477f52
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-11-21 12:19:06 +0700

    tab commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
-let menuBtn = document.querySelector('.menu-btn')
-let menu = document.querySelector('.mobile-navigation')
-
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.mobile-navigation');
 
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active')
     menuBtn.classList.toggle('active');
 });
+
+function selectTab(tabIndex){
+    document.getElementById('tab1Content').style.display="none";
+    document.getElementById('tab2Content').style.display="none";
+    document.getElementById('tab3Content').style.display="none";
+    document.getElementById('tab4Content').style.display="none";
+    document.getElementById('tab5Content').style.display="none";
+    document.getElementById('tab6Content').style.display="none";
+
+    document.getElementById('tab' + tabIndex + 'Content').style.display="flex";
+}
+
