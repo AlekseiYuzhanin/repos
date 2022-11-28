2dae6ffd3bdd68940a8ba2e675b51238d1c6145f
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-11-28 13:17:58 +0700

    form commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.mobile-navigation');
+let nameInput = document.getElementById('nameId');
+let numberPhone = document.getElementById('numberPhoneId');
+let footerBtn = document.querySelector('.middle-element__btn');
 
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active')
@@ -17,3 +20,21 @@ function selectTab(tabIndex){
     document.getElementById('tab' + tabIndex + 'Content').style.display="flex";
 }
 
+let contactForm = document.querySelector('#contactForm')
+
+function handlerFormSubmit(event) {
+    event.preventDefault();
+    getDataForm(contactForm);
+}
+// данные с формы
+function getDataForm(formNode) {
+    let elements = formNode;
+    let data = Array.from(elements)
+        .filter((item) => !!item.name)
+        .map((element) => {
+            const { name, value } = element;
+            return { name, value };
+        });
+    console.log(data);
+}
+contactForm.addEventListener("submit", handlerFormSubmit);
